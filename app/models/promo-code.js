a3773a9f80ad56293329c7c2a9efe70a4f56445d
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-05-30 18:26:48 +0200

    Added basic models and ToS.

---
+++ app/models/promo-code.js
@@ -0,0 +1,4 @@
+export default DS.Model.extend({
+  active: DS.attr('boolean'),
+  discount: DS.attr('number')
+});
---
+++ app/models/purchase.js
@@ -0,0 +1,5 @@
+export default DS.Model.extend({
+  invoiceData: DS.attr('string'),
+  promoCodeId: DS.attr('string'),
+  ticketType: DS.attr('string')
+});
