e81e4840b82f458cea12a1b009747b5d6bbae223
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-08-20 20:40:06 +0200

    Marking workshop selection on time slots.

--- app/components/agenda-workshops-section.js
+++ app/components/agenda-workshops-section.js
@@ -27,6 +27,41 @@ export default Ember.Component.extend({
     });
   }.observes('workshops'),
 
+  workshopSelectedInThisTimeSlot: function () {
+    var component = this;
+    var selected = false;
+    this.get('parentController.selectedWorkshops')
+      .forEach(function (workshop) {
+        if (workshop.get('day') === component.get('day')) {
+          workshop.get('timeSlots').forEach(function (timeSlot) {
+            if (timeSlot.get('order') === component.get('timeSlot')) {
+              selected = true;
+            }
+          });
+        }
+      });
+    return selected;
+  }.property('parentController.selectedWorkshops'),
+
+  isLunchSelected: function () {
+    var component = this;
+    if (component.get('timeSlot') === 1 || component.get('timeSlot') === 2) {
+      var colliding = false;
+      this.get('parentController.selectedWorkshops').forEach(function(workshop){
+        workshop.get('timeSlots').forEach(function(timeSlot){
+          if (timeSlot.get('day') === component.get('day')) {
+            if ((component.get('timeSlot') === 1 && timeSlot.get('order') === 2) || (component.get('timeSlot') === 2 && timeSlot.get('order') === 1)) {
+              colliding = true;
+            }
+          }
+        });
+      });
+      return colliding;
+    } else {
+      return false;
+    }
+  }.property('parentController.selectedWorkshops'),
+
   setExpanded: function (expanded) {
     var height = expanded ? this.$('.workshop-list ul').height() : 0;
     this.$('.workshop-list').css('max-height', height + 'px');
@@ -51,7 +86,7 @@ export default Ember.Component.extend({
       $('.reveal-modal#' + workshop.get('id')).foundation('reveal', 'open');
     },
 
-    selectWorkshop: function(workshop) {
+    selectWorkshop: function (workshop) {
       this.sendAction('selectWorkshop', workshop);
     }
   }
