c8a326f76cefade43cc1f0f4b89913f56914182d
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2015-09-01 00:24:05 +0200

    Almost ready.

--- app/components/input-rating-widget.js
+++ app/components/input-rating-widget.js
@@ -4,18 +4,45 @@ export default Ember.Component.extend({
   alreadySent: false,
 
   sendFeedback: function () {
+    this.set('alreadySent', false);
+    this.$('.overlay').removeClass('hidden');
+
+    console.log(this.get('rating'));
     console.log(this.get('questionId'));
     console.log(this.get('userId'));
     console.log(this.get('userToken'));
-    console.log(this.get('value'));
+    console.log(this.get('comment'));
+    console.log(this.get('workshopId'));
+    console.log(this.get('mentorId'));
 
-    this.set('alreadySent', false);
-    this.$('.overlay').removeClass('hidden');
+    var data = {
+      feedbackId: {
+        questionId: this.get('questionId'),
+        userId: this.get('userId'),
+        userToken: this.get('userToken')
+      },
+      answer: this.get('rating'),
+      comment: this.get('comment'),
+      workshopId: this.get('workshopId'),
+      mentorId: this.get('mentorId')
+    };
+
     var component = this;
-    Ember.run.later(function () {
-      component.$('.overlay').addClass('hidden');
-      component.set('alreadySent', true);
-    }, 1000);
+    Ember.$
+      .ajax({
+        url: 'http://codepot.tk:8081/feedback',
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        dataType: 'json'
+      })
+      .fail(function (error) {
+        console.error(error);
+      })
+      .always(function () {
+        component.$('.overlay').addClass('hidden');
+        component.set('alreadySent', true);
+      });
   },
 
   actions: {
